f2afc26763ec55e95f3f8cd8c0831d6df50fb4ff
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-10 16:28:14 -0300

    Atualizando uso do Swagger no projeto
    Atualizando uso de Swagger no projeto para utilizar as melhores práticas, usos inteligentes e ferramentas para ajudar

--- OneBus.API/Program.cs
+++ OneBus.API/Program.cs
@@ -1,23 +1,33 @@
-using Ardalis.Result.AspNetCore;
-using Microsoft.EntityFrameworkCore;
-using OneBus.Infra.Data.DbContexts;
+using System.Text;
 using OneBus.Infra.Ioc;
+using Microsoft.OpenApi.Models;
+using OneBus.Infra.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services
-    .AddControllers(mvcOptions => mvcOptions.AddDefaultResultConvention())
-    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+    .AddControllers()
+    .AddJsonOptions(c => c.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
-// appsettings Configurations
-var isDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER")?.Equals("true") ?? false;
+// Configuring json visualization for Enums 
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 
+// appsettings configurations
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+var isDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER")?.Equals("true") ?? false;
 
 if (isDocker)
 {
@@ -30,27 +40,84 @@ builder.Configuration.AddEnvironmentVariables();
 // Adds IoC configurations
 builder.Services.AddInfrastructure();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Add Swagger with JWT Bearer configuration
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "OneBus API", Version = "v1" });
+
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "OneBus.API.xml"));
+
+    c.SupportNonNullableReferenceTypes();
+    c.UseAllOfToExtendReferenceSchemas();
+
+    // Apply default value into Enums
+    c.UseInlineDefinitionsForEnums();
+
+    // Add JWT Bearer configuration to Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
+// Authentication settings
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["TokenConfigurations:Issuer"],
+        ValidAudience = builder.Configuration["TokenConfigurations:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["TokenConfigurations:Key"]!))
+    };
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.UseInlineDefinitionsForEnums();
-});
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 // Add DbContext Service
 builder.Services.AddDbContext<OneBusDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"), 
-        sqlServerOpts => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"),
+        sqlServerOpts =>
         {
             sqlServerOpts.MigrationsAssembly(typeof(OneBusDbContext).Assembly);
             sqlServerOpts.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), null);
         }
-    );    
+    );
 });
 
 builder.Services.AddCors(o => o.AddPolicy("*", builder =>
@@ -72,9 +139,13 @@ using (IServiceScope scope = app.Services.CreateScope())
 app.MapOpenApi();
 
 app.UseSwagger();
-app.UseSwaggerUI(options =>
+app.UseSwaggerUI(c =>
 {
-    options.SwaggerEndpoint("/openapi/v1.json", "OneBus API");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "OneBus API v1");
+    c.RoutePrefix = "swagger";
+    c.DisplayRequestDuration();
+    c.EnableDeepLinking();
+    c.EnablePersistAuthorization();
 });
 
 app.UseHttpsRedirection();
