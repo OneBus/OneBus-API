40c22e14f1db252c764b1c50f4bebf18e1d875c8
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-11 17:44:11 -0300

    Adição de classe que representa Erro
    Adição de classe Erro que possui campo Mensagem e Campo, será útil para identificar erros que surgirem

---
+++ OneBus.Domain/Commons/Error.cs
@@ -0,0 +1,15 @@
+﻿namespace OneBus.Domain.Commons
+{
+    public class Error
+    {
+        public Error(string message, string field)
+        {
+            Message = message;
+            Field = field;
+        }
+
+        public string Message { get; }
+
+        public string Field { get; }
+    }
+}
