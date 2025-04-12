69c4447735d2c9b69164d9bbcd346ea95fc57910
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-12 13:24:17 -0300

    Adição de classe ErrorUtils
    Adição de classe ErrorUtils para facilitar criação de erros para cenários em comum e facilitar leitura de código

---
+++ OneBus.Domain/Commons/ErrorUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace OneBus.Domain.Commons
+{
+    public static class ErrorUtils
+    {
+        /// <summary>
+        /// Entidade não encontrada.
+        /// </summary>
+        /// <returns></returns>
+        public static Error EntityNotFound()
+        {
+            return new Error("Entidade não encontrada.", "id");
+        }
+
+        /// <summary>
+        /// Id da rota e Id do corpo estão diferentes.
+        /// </summary>
+        /// <returns></returns>
+        public static Error IdConflict() 
+        {
+            return new Error("Id da rota e Id do corpo estão diferentes.", "ids");
+        }
+    }
+}
