bd5dca9a7f80e3975a8aeed2dba7b8fee219280a
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:52:54 -0300

    Adição de Controller para Linha
    Adição de Controller para linha com Endpoints CRUD

---
+++ OneBus.API/Controllers/LineController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.Line;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class LineController : BaseController<Line, CreateLineDTO, ReadLineDTO, UpdateLineDTO, BaseFilter>
+    {
+        public LineController(
+            IBaseService<Line, CreateLineDTO, ReadLineDTO, UpdateLineDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
