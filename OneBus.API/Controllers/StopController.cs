3818504a6329682b45d96e312e498a369f1663fb
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:55:47 -0300

    Adição de Controller para Parada
    Adição de Controller para parada com Endpoints CRUD

---
+++ OneBus.API/Controllers/StopController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.Stop;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class StopController : BaseController<Stop, CreateStopDTO, ReadStopDTO, UpdateStopDTO, BaseFilter>
+    {
+        public StopController(
+            IBaseService<Stop, CreateStopDTO, ReadStopDTO, UpdateStopDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
