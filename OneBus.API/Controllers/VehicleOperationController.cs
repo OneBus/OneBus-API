ed573bcb2636b745de745c7a082656ba8211fac1
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:58:16 -0300

    Adição de Controller para Operação Veículo
    Adição de Controller para operação veículo com Endpoints CRUD

---
+++ OneBus.API/Controllers/VehicleOperationController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.VehicleOperation;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class VehicleOperationController : BaseController<VehicleOperation, CreateVehicleOperationDTO, ReadVehicleOperationDTO, UpdateVehicleOperationDTO, BaseFilter>
+    {
+        public VehicleOperationController(
+            IBaseService<VehicleOperation, CreateVehicleOperationDTO, ReadVehicleOperationDTO, UpdateVehicleOperationDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
