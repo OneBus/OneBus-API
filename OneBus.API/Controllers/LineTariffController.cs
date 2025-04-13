74b3a0083a0820e7105a38b794fd393b459726c9
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:54:12 -0300

    Adição de Controller para Tarifa Linha
    Adição de Controller para tarifa linha com Endpoints CRUD

---
+++ OneBus.API/Controllers/LineTariffController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.LineTariff;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class LineTariffController : BaseController<LineTariff, CreateLineTariffDTO, ReadLineTariffDTO, UpdateLineTariffDTO, BaseFilter>
+    {
+        public LineTariffController(
+            IBaseService<LineTariff, CreateLineTariffDTO, ReadLineTariffDTO, UpdateLineTariffDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
