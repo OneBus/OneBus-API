c72391f07790a049f2fe19200e39e9ecdb7ebd9d
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:51:06 -0300

    Adição de Controller para Funcionário
    Adição de Controller para funcionário com Endpoints CRUD

---
+++ OneBus.API/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.Employee;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class EmployeeController : BaseController<Employee, CreateEmployeeDTO, ReadEmployeeDTO, UpdateEmployeeDTO, BaseFilter>
+    {
+        public EmployeeController(
+            IBaseService<Employee, CreateEmployeeDTO, ReadEmployeeDTO, UpdateEmployeeDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
