5029033348937d2e5b9e8cc820492920640b44cc
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:53:46 -0300

    Adição de Controller para Horário Linha
    Adição de Controller para horário linha com Endpoints CRUD

---
+++ OneBus.API/Controllers/LineTimeController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.LineTime;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class LineTimeController : BaseController<LineTime, CreateLineTimeDTO, ReadLineTimeDTO, UpdateLineTimeDTO, BaseFilter>
+    {
+        public LineTimeController(
+            IBaseService<LineTime, CreateLineTimeDTO, ReadLineTimeDTO, UpdateLineTimeDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
