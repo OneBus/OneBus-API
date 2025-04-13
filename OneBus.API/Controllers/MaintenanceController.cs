dd05720a57d636463ec15029232746035b289c8a
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:55:28 -0300

    Adição de Controller para Manutenção
    Adição de Controller para manutenção com Endpoints CRUD

---
+++ OneBus.API/Controllers/MaintenanceController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.Maintenance;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class MaintenanceController : BaseController<Maintenance, CreateMaintenanceDTO, ReadMaintenanceDTO, UpdateMaintenanceDTO, BaseFilter>
+    {
+        public MaintenanceController(
+            IBaseService<Maintenance, CreateMaintenanceDTO, ReadMaintenanceDTO, UpdateMaintenanceDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
