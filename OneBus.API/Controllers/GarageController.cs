6d97140291baa53c4196415b071434a2fbea44d8
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:52:31 -0300

    Adição de Controller para Garagem
    Adição de Controller para garagem com Endpoints CRUD

---
+++ OneBus.API/Controllers/GarageController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.Garage;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class GarageController : BaseController<Garage, CreateGarageDTO, ReadGarageDTO, UpdateGarageDTO, BaseFilter>
+    {
+        public GarageController(
+            IBaseService<Garage, CreateGarageDTO, ReadGarageDTO, UpdateGarageDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
