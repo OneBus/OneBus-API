c4e490bc4548202eb89bb7e6b598a8937bf014fb
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:55:02 -0300

    Adição de Controller para Auditoria Tarifa Linha
    Adição de Controller para auditoria tarifa linha com Endpoints CRUD

---
+++ OneBus.API/Controllers/LineTariffAuditController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.LineTariffAudit;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class LineTariffAuditController : BaseController<LineTariffAudit, CreateLineTariffAuditDTO, ReadLineTariffAuditDTO, UpdateLineTariffAuditDTO, BaseFilter>
+    {
+        public LineTariffAuditController(
+            IBaseService<LineTariffAudit, CreateLineTariffAuditDTO, ReadLineTariffAuditDTO, UpdateLineTariffAuditDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
