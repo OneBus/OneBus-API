ab8d9be2ce86f92491c735a9892dcae778156dea
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:50:23 -0300

    Adição de Controller para Empresa
    Adição de Controller para empresa com Endpoints CRUD

---
+++ OneBus.API/Controllers/CompanyController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.Company;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class CompanyController : BaseController<Company, CreateCompanyDTO, ReadCompanyDTO, UpdateCompanyDTO, BaseFilter>
+    {
+        public CompanyController(
+            IBaseService<Company, CreateCompanyDTO, ReadCompanyDTO, UpdateCompanyDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
