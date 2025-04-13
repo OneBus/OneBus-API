69755f3e6dc1c6863c5cdfd47aea6b75ab67ee01
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:53:16 -0300

    Adição de Controller para Endereço Linha
    Adição de Controller para endereço linha com Endpoints CRUD

---
+++ OneBus.API/Controllers/LineAddressController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.LineAddress;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class LineAddressController : BaseController<LineAddress, CreateLineAddressDTO, ReadLineAddressDTO, UpdateLineAddressDTO, BaseFilter>
+    {
+        public LineAddressController(
+            IBaseService<LineAddress, CreateLineAddressDTO, ReadLineAddressDTO, UpdateLineAddressDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
