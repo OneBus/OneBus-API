8017b6ce0f63b16b168cdd3f5e6c9c1376b839c0
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:56:09 -0300

    Adição de Controller para Horário Parada
    Adição de Controller para horário parada com Endpoints CRUD

---
+++ OneBus.API/Controllers/StopTimeController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.StopTime;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class StopTimeController : BaseController<StopTime, CreateStopTimeDTO, ReadStopTimeDTO, UpdateStopTimeDTO, BaseFilter>
+    {
+        public StopTimeController(
+            IBaseService<StopTime, CreateStopTimeDTO, ReadStopTimeDTO, UpdateStopTimeDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
