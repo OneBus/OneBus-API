84e670324516dca146b1c349c7269caf16d8feaf
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:52:04 -0300

    Adição de Controller para Horário Funcionário
    Adição de Controller para horário funcionário com Endpoints CRUD

---
+++ OneBus.API/Controllers/EmployeeWorkdayController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.EmployeeWorkday;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class EmployeeWorkdayController : BaseController<EmployeeWorkday, CreateEmployeeWorkdayDTO, ReadEmployeeWorkdayDTO, UpdateEmployeeWorkdayDTO, BaseFilter>
+    {
+        public EmployeeWorkdayController(
+            IBaseService<EmployeeWorkday, CreateEmployeeWorkdayDTO, ReadEmployeeWorkdayDTO, UpdateEmployeeWorkdayDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
