da7275e6cce4c8cf19fc17067d6b0986b30a6524
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:56:47 -0300

    Adição de Controller para Usuário
    Adição de Controller para usuário com Endpoints CRUD

---
+++ OneBus.API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.User;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class UserController : BaseController<User, CreateUserDTO, ReadUserDTO, UpdateUserDTO, BaseFilter>
+    {
+        public UserController(
+            IBaseService<User, CreateUserDTO, ReadUserDTO, UpdateUserDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
