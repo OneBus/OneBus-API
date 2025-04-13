6bdb431b90961ea8a7ae3f290b9f06fa3318d14b
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-13 15:57:47 -0300

    Adição de Controller para Garagem Veículo
    Adição de Controller para garagem veículo com Endpoints CRUD

---
+++ OneBus.API/Controllers/VehicleGarageController.cs
@@ -0,0 +1,16 @@
+﻿using OneBus.Application.DTOs.VehicleGarage;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+
+namespace OneBus.API.Controllers
+{
+    public class VehicleGarageController : BaseController<VehicleGarage, CreateVehicleGarageDTO, ReadVehicleGarageDTO, UpdateVehicleGarageDTO, BaseFilter>
+    {
+        public VehicleGarageController(
+            IBaseService<VehicleGarage, CreateVehicleGarageDTO, ReadVehicleGarageDTO, UpdateVehicleGarageDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+    }
+}
