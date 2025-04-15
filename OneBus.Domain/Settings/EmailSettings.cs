5f9ac6d0e36f1f9f02aade7fb098922ab941bd33
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-15 17:33:24 -0300

    Adição de classe de Configuração de Email
    Adição de classe de Configuração de Email para envio via SMTP

---
+++ OneBus.Domain/Settings/EmailSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace OneBus.Domain.Settings
+{
+    public class EmailSettings
+    {
+        public EmailSettings()
+        {
+            MailUser = string.Empty;
+            MailPassword = string.Empty;
+            MailHost = string.Empty;
+            MailPort = string.Empty;
+            MailEnableSsl = string.Empty;
+        }
+        
+        // SMTP Mail Service Configuration
+        public string MailUser { get; set; }
+
+        public string MailPassword { get; set; }
+
+        public string MailHost { get; set; }
+
+        public string MailPort { get; set; }
+
+        public string MailEnableSsl { get; set; }
+    }
+}
