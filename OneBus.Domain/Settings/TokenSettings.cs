ddbefe52b738ffe92f29b6a6a2fd07c7354bbff2
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-15 17:34:00 -0300

    Adição de classe de Configuração de Token'
    Adição de classe de Configuração de Token tipo JWT

---
+++ OneBus.Domain/Settings/TokenSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace OneBus.Domain.Settings
+{
+    public class TokenSettings
+    {
+        public TokenSettings()
+        {
+            Audience = string.Empty;
+            Issuer = string.Empty;
+        }
+
+        public string Audience { get; set; }
+
+        public string Issuer { get; set; }
+    }
+}
