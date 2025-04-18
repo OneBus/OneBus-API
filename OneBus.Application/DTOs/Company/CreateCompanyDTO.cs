5caa1451e1934af87c8df70b8965012d226b0f51
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-18 17:25:31 -0300

    Populando DTO de CreateCompany
    Adição dos dados que compõem a criação de Empresa

--- OneBus.Application/DTOs/Company/CreateCompanyDTO.cs
+++ OneBus.Application/DTOs/Company/CreateCompanyDTO.cs
@@ -1,6 +1,23 @@
-﻿namespace OneBus.Application.DTOs.Company
+﻿using OneBus.Application.DTOs.User;
+
+namespace OneBus.Application.DTOs.Company
 {
     public class CreateCompanyDTO : BaseCreateDTO
     {
+        public CreateCompanyDTO()
+        {
+            Name = string.Empty;
+            Cnpj = string.Empty;
+            Email = string.Empty;
+            CreateUser = default!;
+        }
+
+        public string Name { get; set; }
+
+        public string Cnpj { get; set; }
+
+        public string Email { get; set; }
+
+        public CreateUserDTO CreateUser { get; set; }           
     }
 }
