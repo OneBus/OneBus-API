fa3c988d4062cc03173888f9f53d30acfe1ed47b
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-18 17:26:02 -0300

    Populando DTO de CreateUser
    Adição dos dados que compõem a criação de Usuário

--- OneBus.Application/DTOs/User/CreateUserDTO.cs
+++ OneBus.Application/DTOs/User/CreateUserDTO.cs
@@ -2,5 +2,20 @@
 {
     public class CreateUserDTO : BaseCreateDTO
     {
+        public CreateUserDTO()
+        {
+            Name = string.Empty;
+            Email = string.Empty;
+            Password = string.Empty;
+            ConfirmPassword = string.Empty; 
+        }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
     }
 }
