1018f11caeef72aaea172405e600535503cb6225
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-18 17:23:09 -0300

    Adição de TokenDTO
    Adição de DTO para manipulação e visualização do Token do Usuário

---
+++ OneBus.Application/DTOs/Token/TokenDTO.cs
@@ -0,0 +1,33 @@
+﻿using OneBus.Domain.Extensions;
+using System.Text.Json.Serialization;
+
+namespace OneBus.Application.DTOs.Token
+{
+    public class TokenDTO
+    {
+        public const int DaysUntilExpire = 7;
+
+        public TokenDTO(ulong companyId, ulong userId, string userEmail)
+        {
+            UserId = userId;
+            CompanyId = companyId;
+            UserEmail = userEmail;
+            
+            Token = string.Empty;
+            Expiration = DateTime.UtcNow.ToBrazilianDateTime().AddDays(DaysUntilExpire);
+        }
+
+        [JsonIgnore]
+        public ulong CompanyId { get; }
+
+        [JsonIgnore]
+        public ulong UserId { get; }
+
+        [JsonIgnore]
+        public string UserEmail { get; }
+
+        public DateTime Expiration { get; }
+        
+        public string Token { get; set; }            
+    }
+}
