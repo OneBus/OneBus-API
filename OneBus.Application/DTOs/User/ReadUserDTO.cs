413840f6312fb8158dc72adb7cec601401d25c2e
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-20 01:00:59 -0300

    Atualizando DTO de Usuário
    Adição de campos em DTO Read

--- OneBus.Application/DTOs/User/ReadUserDTO.cs
+++ OneBus.Application/DTOs/User/ReadUserDTO.cs
@@ -2,5 +2,13 @@
 {
     public class ReadUserDTO : BaseReadDTO
     {
+        public ReadUserDTO()
+        {
+            Email = string.Empty;
+        }
+
+        public string Email { get; set; }
+
+        public ulong CompanyId { get; set; }
     }
 }
