e880b16dfd8f4eab6c8c4cb11550c56b19dd1782
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-20 01:01:27 -0300

    Adição de lógica para geração de Token
    Adição de lógica para gerar Token em TokenService

--- OneBus.Application/Interfaces/Services/ITokenService.cs
+++ OneBus.Application/Interfaces/Services/ITokenService.cs
@@ -1,6 +1,10 @@
-﻿namespace OneBus.Application.Interfaces.Services
+﻿using OneBus.Application.DTOs.User;
+using OneBus.Domain.Models;
+
+namespace OneBus.Application.Interfaces.Services
 {
     public interface ITokenService
     {
+        TokenModel Generate(ReadUserDTO user);
     }
 }
--- OneBus.Application/Services/TokenService.cs
+++ OneBus.Application/Services/TokenService.cs
@@ -1,8 +1,65 @@
-﻿using OneBus.Application.Interfaces.Services;
+﻿using System.Text;
+using OneBus.Domain.Models;
+using OneBus.Domain.Settings;
+using System.Security.Claims;
+using OneBus.Domain.Extensions;
+using System.Security.Principal;
+using Microsoft.Extensions.Options;
+using OneBus.Application.DTOs.User;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using OneBus.Application.Interfaces.Services;
 
 namespace OneBus.Application.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly TokenSettings _tokenSettings;
+
+        public TokenService(IOptions<TokenSettings> options)
+        {
+            _tokenSettings = options.Value;
+        }
+
+        public TokenModel Generate(ReadUserDTO user)
+        {
+            var claims = new List<Claim>
+            {
+                new("id", user.Id.ToString()!),
+                new("company_id", user.CompanyId.ToString()),
+                new(JwtRegisteredClaimNames.Email, user.Email),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N"))
+            };
+
+            ClaimsIdentity identity = new(new GenericIdentity(user.Email, "login"), claims);
+
+            DateTime createdAt = DateTime.UtcNow.ToBrazilianDateTime();
+            DateTime expiresAt = createdAt.AddDays(_tokenSettings.DaysUntilExpires);
+
+            return new TokenModel(Create(
+                key: Encoding.ASCII.GetBytes(_tokenSettings.Key),
+                identity,
+                createdAt,
+                expiresAt)
+            );
+        }
+
+        private string Create(byte[] key, ClaimsIdentity identity, DateTime createdAt, DateTime expiresAt)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            return handler.WriteToken(
+                handler.CreateToken(new SecurityTokenDescriptor
+                {
+                    Issuer = _tokenSettings.Issuer,
+                    Audience = _tokenSettings.Audience,
+                    SigningCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature),
+                    Subject = identity,
+                    NotBefore = createdAt,
+                    Expires = expiresAt,
+                })
+            );
+        }
     }
 }
