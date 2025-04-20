0968a4af63bcb9c6aa87ac97d671744cfdd2c98d
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-20 00:53:38 -0300

    Atualizando TokenSettings
    Adição de campo Dias até Expiração de Token

--- OneBus.Domain/Settings/TokenSettings.cs
+++ OneBus.Domain/Settings/TokenSettings.cs
@@ -6,10 +6,15 @@
         {
             Audience = string.Empty;
             Issuer = string.Empty;
+            Key = string.Empty;
         }
 
         public string Audience { get; set; }
 
         public string Issuer { get; set; }
+
+        public string Key { get; set; }
+
+        public int DaysUntilExpires { get; set; }
     }
 }
