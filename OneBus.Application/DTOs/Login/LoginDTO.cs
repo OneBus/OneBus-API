1e96a171fc4821af6e09b5897e5ae6e827c18e90
Eduardo Rezende <dudumoises2005@gmail.com>
2025-04-20 01:02:07 -0300

    Adição de LoginDTO
    Adição de DTO responsável por representar campos de entrada de dados em Login

---
+++ OneBus.Application/DTOs/Login/LoginDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace OneBus.Application.DTOs.Login
+{
+    public class LoginDTO
+    {
+        public LoginDTO()
+        {
+            Email = string.Empty;
+            Password = string.Empty;
+        }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
