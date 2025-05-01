fe003d95a7204e83dcfad62c0f09d29b81cecf3c
Eduardo Rezende <dudumoises2005@gmail.com>
2025-05-01 14:55:31 -0300

    Atualizando Extensão de Resultados
    Atualizando Extensão de Resultados para Serviços

--- OneBus.Application/Extensions/ResultExtensions.cs
+++ OneBus.Application/Extensions/ResultExtensions.cs
@@ -1,13 +1,19 @@
 ﻿using FluentValidation.Results;
 using OneBus.Domain.Commons;
+using OneBus.Domain.Commons.Result;
 
 namespace OneBus.Application.Extensions
 {
     public static class ResultExtensions
     {
-        public static IEnumerable<Error> ToErrors(this ValidationResult validationResult)
+        public static InvalidResult<T> ToInvalidResult<T>(this ValidationFailure validationFailure)
         {
-            return validationResult.Errors.Select(c => new Error(c.ErrorMessage, c.PropertyName));
+            return InvalidResult<T>.Create(new Error(validationFailure.ErrorMessage, validationFailure.PropertyName));
+        }
+
+        public static InvalidResult<T> ToInvalidResult<T>(this List<ValidationFailure> validationFailures)
+        {
+            return InvalidResult<T>.Create(validationFailures.Select(c => new Error(c.ErrorMessage, c.PropertyName)));
         }
     }
 }
