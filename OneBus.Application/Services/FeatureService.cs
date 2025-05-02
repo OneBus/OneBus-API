1a050cca25a832ff82a24e3e2dcf40e71fc3f247
Eduardo Rezende <dudumoises2005@gmail.com>
2025-05-02 17:12:23 -0300

    Adição de novos Serviços
    Adição de Serviços para TipoUsuario, Funcionalidade e TipoUsuarioFuncionalidade

---
+++ OneBus.Application/Services/FeatureService.cs
@@ -0,0 +1,15 @@
+﻿using OneBus.Application.DTOs.Feature;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+using OneBus.Domain.Interfaces.Repositories;
+
+namespace OneBus.Application.Services
+{
+    public class FeatureService : BaseReadOnlyService<Feature, ReadFeatureDTO, BaseFilter>, IFeatureService
+    {
+        public FeatureService(IBaseReadOnlyRepository<Feature, BaseFilter> baseReadOnlyRepository) : base(baseReadOnlyRepository)
+        {
+        }
+    }
+}
---
+++ OneBus.Application/Services/UserTypeService.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using OneBus.Application.DTOs.UserType;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+using OneBus.Domain.Interfaces.Repositories;
+
+namespace OneBus.Application.Services
+{
+    public class UserTypeService : BaseService<UserType, CreateUserTypeDTO, ReadUserTypeDTO, UpdateUserTypeDTO, BaseFilter>,
+        IUserTypeService
+    {
+        public UserTypeService(
+            IBaseRepository<UserType, BaseFilter> baseRepository, 
+            IValidator<CreateUserTypeDTO> createValidator, 
+            IValidator<UpdateUserTypeDTO> updateValidator) 
+            : base(baseRepository, createValidator, updateValidator)
+        {
+        }
+
+        protected override void UpdateFields(UserType entity, UpdateUserTypeDTO updateDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
