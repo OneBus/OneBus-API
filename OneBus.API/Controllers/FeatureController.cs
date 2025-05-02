ff7dda5cff4fa3ddeb8157ebca4d2cf6f3a3a19a
Eduardo Rezende <dudumoises2005@gmail.com>
2025-05-02 17:12:48 -0300

    Adição de novos Controllers
    Adição de Controllers para TipoUsuario, Funcionalidade e TipoUsuarioFuncionalidade

---
+++ OneBus.API/Controllers/FeatureController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneBus.Application.DTOs.Feature;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Commons.Result;
+using OneBus.Domain.Commons;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace OneBus.API.Controllers
+{
+    [SwaggerTag("Controlador responsável por gerenciar Funcionalidades")]
+    public class FeatureController : BaseReadOnlyController<Feature, ReadFeatureDTO, BaseFilter>
+    {
+        public FeatureController(IBaseReadOnlyService<Feature, ReadFeatureDTO, BaseFilter> baseReadOnlyService) 
+            : base(baseReadOnlyService)
+        {
+        }
+
+        /// <summary>
+        /// Obter funcionalidades paginadas e filtradas
+        /// </summary>
+        /// <remarks>
+        /// GET de Funcionalidades
+        /// </remarks>
+        /// <param name="filter">Filtros para aplicar</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Funcionalidades paginadas e filtradas</returns>
+        /// <response code="200">Funcionalidades paginadas e filtradas com sucesso</response>
+        [ProducesResponseType(typeof(SuccessResult<Pagination<ReadFeatureDTO>>), StatusCodes.Status200OK)]
+        public override Task<IActionResult> GetPaginedAsync([FromQuery] BaseFilter filter, CancellationToken cancellationToken = default)
+        {
+            return base.GetPaginedAsync(filter, cancellationToken);
+        }
+
+        /// <summary>
+        /// Obter funcionalidade por id
+        /// </summary>
+        /// <remarks>
+        /// GET pelo id da funcionalidade
+        /// </remarks>
+        /// <param name="id">Id da funcionalidade</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Funcionalidade encontrada</returns>
+        /// <response code="200">Funcionalidade encontrada com sucesso</response>
+        /// <response code="404">Funcionalidade não encontrada</response>
+        [ProducesResponseType(typeof(SuccessResult<ReadFeatureDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(NotFoundResult<ReadFeatureDTO>), StatusCodes.Status404NotFound)]
+        public override Task<IActionResult> GetByIdAsync([FromRoute] ulong id, CancellationToken cancellationToken = default)
+        {
+            return base.GetByIdAsync(id, cancellationToken);
+        }
+    }
+}
---
+++ OneBus.API/Controllers/UserTypeController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneBus.Application.DTOs.UserType;
+using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Commons;
+using OneBus.Domain.Commons.Result;
+using OneBus.Domain.Entities;
+using OneBus.Domain.Filters;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace OneBus.API.Controllers
+{
+    [SwaggerTag("Controlador responsável por gerenciar Tipos de Usuários")]
+    public class UserTypeController : BaseController<UserType, CreateUserTypeDTO, ReadUserTypeDTO, UpdateUserTypeDTO, BaseFilter>
+    {
+        public UserTypeController(IBaseService<UserType, CreateUserTypeDTO, ReadUserTypeDTO, UpdateUserTypeDTO, BaseFilter> baseService) 
+            : base(baseService)
+        {
+        }
+
+        /// <summary>
+        /// Cadastrar novo tipo de usuário 
+        /// </summary>
+        /// <remarks>
+        /// POST de Tipo de Usuário
+        /// </remarks>
+        /// <param name="createDTO">Campos para cadastrar tipo de usuário</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Tipo de Usuário cadastrado</returns>
+        /// <response code="200">Tipo de Usuário cadastrado com sucesso</response>
+        /// <response code="422">Validação encontrou erros</response>
+        [ProducesResponseType(typeof(SuccessResult<ReadUserTypeDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(InvalidResult<ReadUserTypeDTO>), StatusCodes.Status422UnprocessableEntity)]
+        public override Task<IActionResult> CreateAsync([FromBody] CreateUserTypeDTO createDTO, CancellationToken cancellationToken = default)
+        {
+            return base.CreateAsync(createDTO, cancellationToken);
+        }
+
+        /// <summary>
+        /// Atualizar tipo de usuário 
+        /// </summary>
+        /// <remarks>
+        /// PUT de Tipo de Usuário
+        /// </remarks>
+        /// <param name="id">Id do tipo de usuário</param>
+        /// <param name="updateDTO">Campos para atualizar tipo de usuário</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Tipo de Usuário atualizado</returns>
+        /// <response code="200">Tipo de Usuário atualizado com sucesso</response>
+        /// <response code="422">Validação encontrou erros</response>
+        /// <response code="404">Tipo de Usuário não encontrado</response>
+        /// <response code="409">Conflito entre ids</response>
+        [ProducesResponseType(typeof(SuccessResult<ReadUserTypeDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(InvalidResult<ReadUserTypeDTO>), StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(typeof(NotFoundResult<ReadUserTypeDTO>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ConflictResult<ReadUserTypeDTO>), StatusCodes.Status409Conflict)]
+        public override Task<IActionResult> UpdateAsync([FromRoute] ulong id, [FromBody] UpdateUserTypeDTO updateDTO, CancellationToken cancellationToken = default)
+        {
+            return base.UpdateAsync(id, updateDTO, cancellationToken);
+        }
+
+        /// <summary>
+        /// Habilitar tipo de usuário 
+        /// </summary>
+        /// <remarks>
+        /// PUT para habilitar tipo de usuário 
+        /// </remarks>
+        /// <param name="id">Id do tipo de usuário</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Tipo de Usuário habilitado</returns>
+        /// <response code="200">Tipo de Usuário habilitado com sucesso</response>
+        /// <response code="404">Tipo de Usuário não encontrado</response>
+        [ProducesResponseType(typeof(SuccessResult<bool>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(NotFoundResult<bool>), StatusCodes.Status404NotFound)]
+        public override Task<IActionResult> EnableAsync([FromRoute] ulong id, CancellationToken cancellationToken = default)
+        {
+            return base.EnableAsync(id, cancellationToken);
+        }
+
+        /// <summary>
+        /// Desabilitar tipo de usuário 
+        /// </summary>
+        /// <remarks>
+        /// DELETE de Tipo de Usuário 
+        /// </remarks>
+        /// <param name="id">Id do tipo de usuário</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Tipo de Usuário deletado</returns>
+        /// <response code="200">Tipo de Usuário deletado com sucesso</response>
+        /// <response code="404">Tipo de Usuário não encontrado</response>
+        [ProducesResponseType(typeof(SuccessResult<bool>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(NotFoundResult<bool>), StatusCodes.Status404NotFound)]
+        public override Task<IActionResult> DisableAsync([FromRoute] ulong id, CancellationToken cancellationToken = default)
+        {
+            return base.DisableAsync(id, cancellationToken);
+        }
+
+        /// <summary>
+        /// Obter tipos de usuários paginados e filtrados
+        /// </summary>
+        /// <remarks>
+        /// GET de Tipos de Usuários
+        /// </remarks>
+        /// <param name="filter">Filtros para aplicar</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Tipos de Usuários paginados e filtrados</returns>
+        /// <response code="200">Tipos de Usuários paginados e filtrados com sucesso</response>
+        [ProducesResponseType(typeof(SuccessResult<Pagination<ReadUserTypeDTO>>), StatusCodes.Status200OK)]
+        public override Task<IActionResult> GetPaginedAsync([FromQuery] BaseFilter filter, CancellationToken cancellationToken = default)
+        {
+            return base.GetPaginedAsync(filter, cancellationToken);
+        }
+
+        /// <summary>
+        /// Obter tipo de usuário por id
+        /// </summary>
+        /// <remarks>
+        /// GET pelo id de tipo de usuário
+        /// </remarks>
+        /// <param name="id">Id do tipo de usuário</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Tipo de Usuário encontrado</returns>
+        /// <response code="200">Tipo de Usuário encontrado com sucesso</response>
+        /// <response code="404">Tipo de Usuário não encontrado</response>
+        [ProducesResponseType(typeof(SuccessResult<ReadUserTypeDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(NotFoundResult<ReadUserTypeDTO>), StatusCodes.Status404NotFound)]
+        public override Task<IActionResult> GetByIdAsync([FromRoute] ulong id, CancellationToken cancellationToken = default)
+        {
+            return base.GetByIdAsync(id, cancellationToken);
+        }
+    }
+}
