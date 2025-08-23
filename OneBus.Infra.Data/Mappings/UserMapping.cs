25d602cff23c43285d250355b78fc339a67b4643
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-23 16:35:27 -0300

    ♻️refactor: Update User Mapping
    Updating User Mapping properties

--- OneBus.Infra.Data/Mappings/UserMapping.cs
+++ OneBus.Infra.Data/Mappings/UserMapping.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using OneBus.Domain.Entities;
 
 namespace OneBus.Infra.Data.Mappings
@@ -8,21 +7,12 @@ namespace OneBus.Infra.Data.Mappings
     {
         public override void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.ToTable("Usuario");
             base.Configure(builder);
 
             builder.Property(c => c.Email).HasMaxLength(80);
-            builder.Property(c => c.UserTypeId).HasColumnName("TipoUsuarioId");            
-            builder.Property(c => c.Salt).HasColumnName("Sal").HasMaxLength(25);
-            builder.Property(c => c.Name).HasColumnName("Nome").HasMaxLength(80);
-            builder.Property(c => c.Image).HasColumnName("Foto").HasMaxLength(20);
-            builder.Property(c => c.Phone).HasColumnName("Telefone").HasMaxLength(20);
-            builder.Property(c => c.Password).HasColumnName("Senha").HasMaxLength(50);          
-
-            builder
-                .HasOne(c => c.UserType)
-                .WithMany(c => c.Users)
-                .HasForeignKey(c => c.UserTypeId);
+            builder.Property(c => c.Salt).HasMaxLength(25);
+            builder.Property(c => c.Name).HasMaxLength(80);
+            builder.Property(c => c.Password).HasMaxLength(50);
         }
     }
 }
