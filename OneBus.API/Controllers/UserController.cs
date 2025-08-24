9c7a83c2ce8fc2d27e6044fc98767f7c76f6cdc2
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-24 13:13:02 -0300

    ♻️refactor: Update UserController
    Adding Login endpoint logic

--- OneBus.API/Controllers/UserController.cs
+++ OneBus.API/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using OneBus.API.Extensions;
 using OneBus.Application.DTOs.Login;
 using OneBus.Application.Interfaces.Services;
+using OneBus.Domain.Commons.Result;
+using OneBus.Domain.Models;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace OneBus.API.Controllers
@@ -31,9 +34,12 @@ namespace OneBus.API.Controllers
         /// <returns>Usuário autenticado</returns>
         [AllowAnonymous]
         [HttpPost("logins")]
-        public Task<IActionResult> LoginAsync([FromBody] LoginDTO login, CancellationToken cancellationToken = default)
+        [ProducesResponseType(typeof(SuccessResult<TokenModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(InvalidResult<TokenModel>), StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(typeof(NotFoundResult<TokenModel>), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> LoginAsync([FromBody] LoginDTO login, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return (await _userService.LoginAsync(login, cancellationToken)).ToActionResult();
         }
     }
 }
