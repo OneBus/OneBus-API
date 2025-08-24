8c9ecbf300159dc6b97d8aa7f8f875486c260bd2
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-24 00:45:43 -0300

    ♻️refactor: Update User Mapping
    Adding admin default record

--- OneBus.Infra.Data/Mappings/UserMapping.cs
+++ OneBus.Infra.Data/Mappings/UserMapping.cs
@@ -10,9 +10,22 @@ namespace OneBus.Infra.Data.Mappings
             base.Configure(builder);
 
             builder.Property(c => c.Email).HasMaxLength(80);
-            builder.Property(c => c.Salt).HasMaxLength(25);
+            builder.Property(c => c.Salt).HasMaxLength(32);
             builder.Property(c => c.Name).HasMaxLength(80);
             builder.Property(c => c.Password).HasMaxLength(50);
+
+            // Admin default data:
+            builder.HasData(
+                new User
+                {
+                    Id = 1,
+                    Name = "Administrador",
+                    Email = "onebus@admin",
+                    Password = "YRT66Z4XEJ2SSNaJVDIXQW7uvC8LSvOxDU1sH/Sr/ic=", //onebus@2025
+                    Salt = "c37b6028194d489192aac9391801594a",
+                    CreatedAt = new DateTime(2025, 08, 25, 00, 00, 00, DateTimeKind.Utc),
+                }
+            );
         }
     }
 }
