389e4ffff2beb4404f09fa9aad16ef15f3ac2fc6
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-24 13:08:05 -0300

    ♻️refactor: Update TokenModel
    Adding new fields to model

--- OneBus.Domain/Models/TokenModel.cs
+++ OneBus.Domain/Models/TokenModel.cs
@@ -1,12 +1,50 @@
-﻿namespace OneBus.Domain.Models
+﻿using System.Reflection;
+
+namespace OneBus.Domain.Models
 {
     public class TokenModel
     {
-        public TokenModel(string token)
+        public TokenModel(
+            long userId,
+            string name,
+            string email,
+            string token,
+            DateTime created,
+            DateTime expiration,
+            string refreshToken)
         {
+            Name = name;
+            Email = email;
             Token = token;
+            UserId = userId;
+            Created = created;
+            Expiration = expiration;
+            RefreshToken = refreshToken;
         }
 
+        public long UserId { get; }
+
+        public string Email { get; }
+
+        public string Name { get; }
+
         public string Token { get; }
+
+        public string RefreshToken { get; }
+
+        public string Version
+        {
+            get
+            {
+                var version = Assembly.GetExecutingAssembly().GetName().Version;
+                return $"{version?.Major}.{version?.Minor}.{version?.Build}";
+            }
+        }
+
+        public DateTime Created { get; }
+
+        public DateTime Expiration { get; }
+
+        public double DurationInSeconds { get { return (Expiration - Created).TotalSeconds; } }
     }
 }
