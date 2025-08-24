a12c6da67e690b978cddbbb3d3adabc129d347ca
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-24 13:12:09 -0300

    ✨feat: Add UserService new methd
    Adding Login method

--- OneBus.Application/Interfaces/Services/IUserService.cs
+++ OneBus.Application/Interfaces/Services/IUserService.cs
@@ -1,11 +1,17 @@
-﻿using OneBus.Application.DTOs.User;
+﻿using OneBus.Application.DTOs.Login;
+using OneBus.Application.DTOs.User;
+using OneBus.Domain.Commons.Result;
 using OneBus.Domain.Entities;
 using OneBus.Domain.Filters;
+using OneBus.Domain.Models;
 
 namespace OneBus.Application.Interfaces.Services
 {
     public interface IUserService : 
         IBaseService<User, CreateUserDTO, ReadUserDTO, UpdateUserDTO, BaseFilter>
     {
+        public Task<Result<TokenModel>> LoginAsync(
+            LoginDTO loginDTO,
+            CancellationToken cancellationToken = default);
     }
 }
