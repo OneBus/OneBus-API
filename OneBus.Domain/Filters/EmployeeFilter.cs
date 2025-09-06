efe1d235a6cc6dc5fd9f2ac4575b4e8975ae526d
Eduardo Rezende <dudumoises2005@gmail.com>
2025-09-06 17:45:31 -0300

    ✨feat: Add EmployeeFilter
    Adding filter class for Employee

---
+++ OneBus.Domain/Filters/EmployeeFilter.cs
@@ -0,0 +1,17 @@
+﻿namespace OneBus.Domain.Filters
+{
+    public class EmployeeFilter : BaseFilter
+    {
+        public byte? Status { get; set; }
+        
+        public byte? CnhCategory { get; set; }
+
+        public byte? Role { get; set; }
+
+        public byte? BloodType { get; set; }
+
+        //public DateOnly? HiringDate { get; set; }
+
+        //public DateOnly? CnhExpiration { get; set; }
+    }
+}
