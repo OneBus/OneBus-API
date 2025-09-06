1c5ae45e8e6269f0cc57b2498cc06ccb1f68bc89
Eduardo Rezende <dudumoises2005@gmail.com>
2025-09-06 17:42:26 -0300

    ♻️refactor: Update Employees DTOS
    Adding new DTOs for Enums and updating Create, Read and Update DTOs with fields

--- OneBus.Application/DTOs/Employee/UpdateEmployeeDTO.cs
+++ OneBus.Application/DTOs/Employee/UpdateEmployeeDTO.cs
@@ -2,5 +2,42 @@
 {
     public class UpdateEmployeeDTO : BaseUpdateDTO
     {
+        public UpdateEmployeeDTO()
+        {
+            Name = string.Empty;
+            Rg = string.Empty;
+            Cpf = string.Empty;
+            Code = string.Empty;
+            Email = string.Empty;
+            Phone = string.Empty;
+        }
+
+        public string Name { get; set; }
+
+        public string Rg { get; set; }
+
+        public string Cpf { get; set; }
+
+        public byte? BloodType { get; set; }
+
+        public string Code { get; set; }
+
+        public byte Role { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public DateOnly HiringDate { get; set; }
+
+        public string? CnhNumber { get; set; }
+
+        public byte? CnhCategory { get; set; }
+
+        public DateOnly? CnhExpiration { get; set; }
+
+        public byte Status { get; set; }
+
+        public byte[]? Image { get; set; }
     }
 }
