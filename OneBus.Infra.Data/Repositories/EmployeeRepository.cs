96b4c1b940b55aab735367992b45bd5f16d161ac
Eduardo Rezende <dudumoises2005@gmail.com>
2025-09-06 17:46:07 -0300

    ♻️refactor: Update EmployeeRepository
    Updating class with filter references and logic

--- OneBus.Infra.Data/Repositories/EmployeeRepository.cs
+++ OneBus.Infra.Data/Repositories/EmployeeRepository.cs
@@ -2,13 +2,32 @@
 using OneBus.Domain.Filters;
 using OneBus.Domain.Interfaces.Repositories;
 using OneBus.Infra.Data.DbContexts;
+using System.Linq.Expressions;
 
 namespace OneBus.Infra.Data.Repositories
 {
-    public class EmployeeRepository : BaseRepository<Employee, BaseFilter>, IEmployeeRepository
+    public class EmployeeRepository : BaseRepository<Employee, EmployeeFilter>, IEmployeeRepository
     {
         public EmployeeRepository(OneBusDbContext dbContext) : base(dbContext)
         {
+        }
+
+        protected override Expression<Func<Employee, bool>> ApplyFilter(EmployeeFilter filter)
+        {
+            var value = filter.Value?.ToLower();
+
+            return c =>
+                (filter.BloodType == null || c.BloodType == filter.BloodType) &&
+                (filter.Status == null || c.Status == filter.Status) &&
+                (filter.Role == null || c.Role == filter.Role) &&
+                (filter.CnhCategory == null || c.CnhCategory == filter.CnhCategory) &&
+                (string.IsNullOrWhiteSpace(value) ||
+                ((c.Name.ToLower().Contains(value) ||
+                c.Code.ToLower().Contains(value) ||
+                c.Rg.ToLower().Contains(value) ||
+                c.Cpf.ToLower().Contains(value) ||
+                c.Phone.ToLower().Contains(value) ||
+                c.CnhNumber!.ToLower().Contains(value)) && value != string.Empty));
         }
     }
 }
