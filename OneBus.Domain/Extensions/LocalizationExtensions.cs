4b5edda4586dbad435ddc1049d493ac83eb67a61
Eduardo Rezende <dudumoises2005@gmail.com>
2025-09-20 16:52:04 -0300

    ✨feat: Add LocalizationExtensions
    Adding extension to easily localize values

---
+++ OneBus.Domain/Extensions/LocalizationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using OneBus.Domain.Commons;
+
+namespace OneBus.Domain.Extensions
+{
+    public static class LocalizationExtensions
+    {
+        public static string Localize(this string? value)
+        {
+            return ResourceMgr.GetString(value, returnKeyWhenNotFound: true);
+        }
+    }
+}
