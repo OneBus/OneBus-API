0df9a8e7a2939f907f7ed15fd6e0023e1acdaed5
Eduardo Rezende <dudumoises2005@gmail.com>
2025-09-20 17:04:00 -0300

    ✨feat: Update EmployeeWorkday Validators
    Adding validation class for Create and Update DTOs

--- OneBus.Application/Validators/EmployeeWorkday/CreateEmployeeWorkdayDTOValidator.cs
+++ OneBus.Application/Validators/EmployeeWorkday/CreateEmployeeWorkdayDTOValidator.cs
@@ -1,9 +1,42 @@
 ﻿using FluentValidation;
 using OneBus.Application.DTOs.EmployeeWorkday;
+using OneBus.Application.Utils;
+using OneBus.Domain.Commons;
+using OneBus.Domain.Enums;
+using OneBus.Domain.Interfaces.Repositories;
 
 namespace OneBus.Application.Validators.EmployeeWorkday
 {
     public class CreateEmployeeWorkdayDTOValidator : AbstractValidator<CreateEmployeeWorkdayDTO>
     {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        const string EmployeeIdPropertyName = "Id do Funcionário";
+
+        public CreateEmployeeWorkdayDTOValidator(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+
+            RuleFor(c => c.EmployeeId).GreaterThan(0)
+                .OverridePropertyName(EmployeeIdPropertyName);
+
+            RuleFor(c => c.EmployeeId)
+                .MustAsync(ExistsAsync)
+                .WithMessage(ErrorUtils.EntityNotFound(EmployeeIdPropertyName).Message)
+                .OverridePropertyName(EmployeeIdPropertyName);
+
+            RuleFor(c => c.DayType).Must(ValidationUtils.IsValidEnumValue<DayType>)
+               .OverridePropertyName("Dia da Semana");
+
+            RuleFor(c => c.EndTime)
+                .Must((dto, endTime) => endTime > dto.StartTime)
+                .WithMessage("Horário inválido")
+                .OverridePropertyName("Horário de Saída");
+        }
+
+        private async Task<bool> ExistsAsync(long employeeId, CancellationToken ct = default)
+        {
+            return await _employeeRepository.AnyAsync(c => c.Id == employeeId, cancellationToken: ct);
+        }
     }
 }
--- OneBus.Application/Validators/EmployeeWorkday/UpdateEmployeeWorkdayDTOValidator.cs
+++ OneBus.Application/Validators/EmployeeWorkday/UpdateEmployeeWorkdayDTOValidator.cs
@@ -4,6 +4,13 @@ using OneBus.Application.DTOs.EmployeeWorkday;
 namespace OneBus.Application.Validators.EmployeeWorkday
 {
     public class UpdateEmployeeWorkdayDTOValidator : AbstractValidator<UpdateEmployeeWorkdayDTO>
-    {
+    {        
+        public UpdateEmployeeWorkdayDTOValidator()
+        {
+            RuleFor(c => c.EndTime)
+                .Must((dto, endTime) => endTime > dto.StartTime)
+                .WithMessage("Horário inválido")
+                .OverridePropertyName("Horário de Saída");
+        }
     }
 }
