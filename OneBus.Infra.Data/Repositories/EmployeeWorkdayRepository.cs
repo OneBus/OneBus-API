5b2457a317a89e81fff718d964f755ae957305ee
Eduardo Rezende <dudumoises2005@gmail.com>
2025-09-20 16:54:36 -0300

    ✨feat: Add EmployeeWorkday Repository
    Adding repository class for EmployeeWorkday

--- OneBus.Infra.Data/Repositories/EmployeeWorkdayRepository.cs
+++ OneBus.Infra.Data/Repositories/EmployeeWorkdayRepository.cs
@@ -2,13 +2,29 @@
 using OneBus.Domain.Filters;
 using OneBus.Domain.Interfaces.Repositories;
 using OneBus.Infra.Data.DbContexts;
+using System.Linq.Expressions;
 
 namespace OneBus.Infra.Data.Repositories
 {
-    public class EmployeeWorkdayRepository : BaseRepository<EmployeeWorkday, BaseFilter>, IEmployeeWorkdayRepository
+    public class EmployeeWorkdayRepository : BaseRepository<EmployeeWorkday, EmployeeWorkDayFilter>, IEmployeeWorkdayRepository
     {
         public EmployeeWorkdayRepository(OneBusDbContext dbContext) : base(dbContext)
         {
+        }
+
+        protected override Expression<Func<EmployeeWorkday, bool>> ApplyFilter(EmployeeWorkDayFilter filter)
+        {
+            var value = filter.Value?.ToLower();
+
+            return c =>
+                (filter.DayType == null || c.DayType == filter.DayType) &&
+                (string.IsNullOrWhiteSpace(value) ||
+                ((c.Employee!.Name.ToLower().Contains(value) ||
+                c.Employee!.Code.ToLower().Contains(value) ||
+                c.Employee!.Rg.ToLower().Contains(value) ||
+                c.Employee!.Cpf.ToLower().Contains(value) ||
+                c.Employee!.Phone.ToLower().Contains(value) ||
+                c.Employee!.CnhNumber!.ToLower().Contains(value)) && value != string.Empty));
         }
     }
 }
