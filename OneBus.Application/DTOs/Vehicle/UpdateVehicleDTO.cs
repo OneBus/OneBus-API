c4c119866570c5ebfb9f4279d678e058e9cec202
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-04 20:13:36 -0300

    ♻️refactor: Update vehicle DTOs
    Adding fields into DTOs classes

--- OneBus.Application/DTOs/Vehicle/UpdateVehicleDTO.cs
+++ OneBus.Application/DTOs/Vehicle/UpdateVehicleDTO.cs
@@ -2,5 +2,71 @@
 {
     public class UpdateVehicleDTO : BaseUpdateDTO
     {
+        public UpdateVehicleDTO()
+        {
+            Prefix = string.Empty;
+            Brand = string.Empty;
+            Model = string.Empty;
+            Plate = string.Empty;
+            Renavam = string.Empty;
+        }
+
+        public string Prefix { get; set; }
+
+        public byte NumberDoors { get; set; }
+
+        public byte NumberSeats { get; set; }
+
+        public bool HasAccessibility { get; set; }
+
+        public byte FuelType { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public ushort Year { get; set; }
+
+        public string Plate { get; set; }
+
+        public string? Color { get; set; }
+
+        public string? BodyworkNumber { get; set; }
+
+        public string? NumberChassis { get; set; }
+
+        public string? EngineNumber { get; set; }
+
+        public byte AxesNumber { get; set; }
+
+        public DateOnly IpvaExpiration { get; set; }
+
+        public DateOnly LicensingExpiration { get; set; }
+
+        public string Renavam { get; set; }
+
+        public byte TransmissionType { get; set; }
+
+        public DateOnly AcquisitionDate { get; set; }
+
+        public byte Status { get; set; }
+
+        public byte[]? Image { get; set; }
+
+        public byte? BusServiceType { get; set; }
+
+        public string? BusChassisBrand { get; set; }
+
+        public string? BusChassisModel { get; set; }
+
+        public short? BusChassisYear { get; set; }
+
+        public bool? BusHasLowFloor { get; set; }
+
+        public bool? BusHasLeftDoors { get; set; }
+
+        public DateOnly? BusInsuranceExpiration { get; set; }
+
+        public DateOnly? BusFumigateExpiration { get; set; }
     }
 }
