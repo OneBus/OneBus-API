4f7b924ea90fa6bef72875f649b79cca13a7e9d6
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-04 20:13:54 -0300

    ♻️refactor: Update Vehicle Service
    Adding UpdateFields method

--- OneBus.Application/Services/VehicleService.cs
+++ OneBus.Application/Services/VehicleService.cs
@@ -20,7 +20,35 @@ namespace OneBus.Application.Services
 
         protected override void UpdateFields(Vehicle entity, UpdateVehicleDTO updateDTO)
         {
-            throw new NotImplementedException();
+            entity.Year = updateDTO.Year;
+            entity.Brand = updateDTO.Brand;
+            entity.Model = updateDTO.Model;
+            entity.Plate = updateDTO.Plate;
+            entity.Color = updateDTO.Color;
+            entity.Image = updateDTO.Image;
+            entity.Prefix = updateDTO.Prefix;
+            entity.Status = updateDTO.Status;
+            entity.Renavam = updateDTO.Renavam;
+            entity.FuelType = updateDTO.FuelType;
+            entity.AxesNumber = updateDTO.AxesNumber;
+            entity.NumberDoors = updateDTO.NumberDoors;
+            entity.NumberSeats = updateDTO.NumberSeats;
+            entity.EngineNumber = updateDTO.EngineNumber;
+            entity.NumberChassis = updateDTO.NumberChassis;
+            entity.BusChassisYear = updateDTO.BusChassisYear;
+            entity.BusServiceType = updateDTO.BusServiceType;
+            entity.BusHasLowFloor = updateDTO.BusHasLowFloor;
+            entity.IpvaExpiration = updateDTO.IpvaExpiration;
+            entity.BodyworkNumber = updateDTO.BodyworkNumber;
+            entity.BusChassisBrand = updateDTO.BusChassisBrand;
+            entity.BusChassisModel = updateDTO.BusChassisModel;
+            entity.AcquisitionDate = updateDTO.AcquisitionDate;
+            entity.BusHasLeftDoors = updateDTO.BusHasLeftDoors;
+            entity.HasAccessibility = updateDTO.HasAccessibility;
+            entity.TransmissionType = updateDTO.TransmissionType;
+            entity.LicensingExpiration = updateDTO.LicensingExpiration;
+            entity.BusFumigateExpiration = updateDTO.BusFumigateExpiration;
+            entity.BusInsuranceExpiration = updateDTO.BusInsuranceExpiration;
         }
     }
 }
