44c8a23df170aeb15e35372b1b1c6f70c4256eda
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-04 22:33:01 -0300

    ✨feat: Add VehicleFilter
    Adding filter class for Vehicle

---
+++ OneBus.Domain/Filters/VehicleFilter.cs
@@ -0,0 +1,21 @@
+﻿namespace OneBus.Domain.Filters
+{
+    public class VehicleFilter : BaseFilter
+    {
+        public byte? Status { get; set; }
+
+        public byte? Type { get; set; }
+
+        public byte? Brand { get; set; }
+
+        public byte? BusChassisBrand { get; set; }
+
+        public byte? BusServiceType { get; set; }
+
+        public byte? Color { get; set; }
+
+        public byte? FuelType { get; set; }
+
+        public byte? TransmissionType { get; set; }
+    }
+}
