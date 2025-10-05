1bc90368b9117981560e5882b6d47e0dbfb34e8d
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-04 22:46:28 -0300

    ♻️refactor: Update Vehicle Repository
    Adding filtering logic

--- OneBus.Infra.Data/Repositories/VehicleRepository.cs
+++ OneBus.Infra.Data/Repositories/VehicleRepository.cs
@@ -2,6 +2,7 @@
 using OneBus.Domain.Filters;
 using OneBus.Domain.Interfaces.Repositories;
 using OneBus.Infra.Data.DbContexts;
+using System.Linq.Expressions;
 
 namespace OneBus.Infra.Data.Repositories
 {
@@ -9,6 +10,30 @@ namespace OneBus.Infra.Data.Repositories
     {
         public VehicleRepository(OneBusDbContext dbContext) : base(dbContext)
         {
+        }
+
+        protected override Expression<Func<Vehicle, bool>> ApplyFilter(VehicleFilter filter)
+        {
+            var value = filter.Value?.ToLower();
+
+            return c =>
+                (filter.Type == null || c.Type == filter.Type) &&
+                (filter.Color == null || c.Color == filter.Color) &&
+                (filter.Brand == null || c.Brand == filter.Brand) &&
+                (filter.Status == null || c.Status == filter.Status) &&
+                (filter.FuelType == null || c.FuelType == filter.FuelType) &&
+                (filter.BusServiceType == null || c.BusServiceType == filter.BusServiceType) &&
+                (filter.BusChassisBrand == null || c.BusChassisBrand == filter.BusChassisBrand) &&
+                (filter.TransmissionType == null || c.TransmissionType == filter.TransmissionType) &&
+                (string.IsNullOrWhiteSpace(value) ||
+                ((c.Prefix.ToLower().Contains(value) ||
+                c.Model.ToLower().Contains(value) ||
+                c.Plate.ToLower().Contains(value) ||
+                c.Renavam.ToLower().Contains(value) ||
+                c.NumberChassis!.ToLower().Contains(value) ||
+                c.BodyworkNumber!.ToLower().Contains(value) ||
+                c.BusChassisModel!.ToLower().Contains(value) ||
+                c.EngineNumber!.ToLower().Contains(value)) && value != string.Empty));
         }
     }
 }
