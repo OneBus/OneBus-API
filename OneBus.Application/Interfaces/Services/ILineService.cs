2924879554e5b5151f57bdabfd547a092eee4e5a
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-07 21:17:25 -0300

    ♻️refactor: Update Line Service
    Adding GET methods logic

--- OneBus.Application/Interfaces/Services/ILineService.cs
+++ OneBus.Application/Interfaces/Services/ILineService.cs
@@ -1,11 +1,14 @@
 ﻿using OneBus.Application.DTOs.Line;
+using OneBus.Domain.Commons.Result;
 using OneBus.Domain.Entities;
 using OneBus.Domain.Filters;
 
 namespace OneBus.Application.Interfaces.Services
 {
-    public interface ILineService : 
-        IBaseService<Line, CreateLineDTO, ReadLineDTO, UpdateLineDTO, BaseFilter>
+    public interface ILineService :
+        IBaseService<Line, CreateLineDTO, ReadLineDTO, UpdateLineDTO, LineFilter>
     {
+        Result<IEnumerable<ReadDirectionTypeDTO>> GetDirectionTypes();
+        Result<IEnumerable<ReadLineTypeDTO>> GetLineTypes();
     }
 }
