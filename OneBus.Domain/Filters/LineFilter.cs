b757b7c3328ed1cfb83670263042f39e481b47a3
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-07 21:12:12 -0300

    ✨feat: Add LineFilter
    Adding filter class for Line

---
+++ OneBus.Domain/Filters/LineFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace OneBus.Domain.Filters
+{
+    public class LineFilter : BaseFilter
+    {
+        public byte? Type { get; set; }
+
+        public byte? DirectionType { get; set; }
+    }
+}
