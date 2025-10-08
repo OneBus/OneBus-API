fc62f9b6a2251430fb51d640423ae66caf8ffe66
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-07 21:13:07 -0300

    ✨feat: Add Migration
    Adding DirectionType field into Line table

---
+++ OneBus.Infra.Data/Migrations/20251007232852_UpdateLineTableFields.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneBus.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateLineTableFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxNumberBuses",
+                table: "Line");
+
+            migrationBuilder.RenameColumn(
+                name: "MinNumberBuses",
+                table: "Line",
+                newName: "DirectionType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DirectionType",
+                table: "Line",
+                newName: "MinNumberBuses");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "MaxNumberBuses",
+                table: "Line",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+    }
+}
