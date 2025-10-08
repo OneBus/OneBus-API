73f79784546258cd91572f22feafde56a636354e
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-07 21:14:46 -0300

    ♻️refactor: Update Line DTOs
    Adding fields to the DTOs

--- OneBus.Application/DTOs/Line/CreateLineDTO.cs
+++ OneBus.Application/DTOs/Line/CreateLineDTO.cs
@@ -2,5 +2,22 @@
 {
     public class CreateLineDTO : BaseCreateDTO
     {
+        public CreateLineDTO()
+        {
+            Name = string.Empty;
+            Number = string.Empty;
+        }
+
+        public string Number { get; set; }
+
+        public string Name { get; set; }
+
+        public byte Type { get; set; }
+
+        public TimeOnly TravelTime { get; set; }
+
+        public decimal Mileage { get; set; }
+
+        public byte DirectionType { get; set; }
     }
 }
---
+++ OneBus.Application/DTOs/Line/ReadDirectionTypeDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace OneBus.Application.DTOs.Line
+{
+    public class ReadDirectionTypeDTO
+    {
+        public ReadDirectionTypeDTO()
+        {
+            Name = string.Empty;
+        }
+
+        public byte Value { get; set; }
+
+        public string Name { get; set; }
+    }
+}
