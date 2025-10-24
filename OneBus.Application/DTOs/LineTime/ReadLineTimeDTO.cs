ad12ab49cb4afa7debe4cb019bc3a3873cb62200
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-23 21:49:49 -0300

    ♻️refactor: Update LineTime DTOs
    Updating DTOs with fields

--- OneBus.Application/DTOs/LineTime/ReadLineTimeDTO.cs
+++ OneBus.Application/DTOs/LineTime/ReadLineTimeDTO.cs
@@ -2,5 +2,20 @@
 {
     public class ReadLineTimeDTO : BaseReadDTO
     {
+        public long LineId { get; set; }
+
+        public string? LineNumber { get; set; }
+
+        public string? LineName { get; set; }
+
+        public TimeOnly Time { get; set; }
+
+        public byte DayType { get; set; }
+
+        public string? DayTypeName { get; set; }
+
+        public byte DirectionType { get; set; }
+
+        public string? DirectionTypeName { get; set; }
     }
 }
