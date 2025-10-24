94fb294945a9223a97c6332f1fe0cee6f9b04d99
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-23 21:46:42 -0300

    ✨feat: Add LineTimeFilter
    Adding filtering fields

---
+++ OneBus.Domain/Filters/LineTimeFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace OneBus.Domain.Filters
+{
+    public class LineTimeFilter : BaseFilter
+    {
+        public byte? DirectionType { get; set; }
+    
+        public byte? DayType { get; set; }
+    }
+}
