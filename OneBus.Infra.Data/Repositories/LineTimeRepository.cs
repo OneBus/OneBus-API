956b87e9d7d774d8a293d3404418eb065aa6e137
Eduardo Rezende <dudumoises2005@gmail.com>
2025-10-23 21:48:53 -0300

    ♻️refactor: Update LineTime Repository
    Adding filtering logic

--- OneBus.Infra.Data/Repositories/LineTimeRepository.cs
+++ OneBus.Infra.Data/Repositories/LineTimeRepository.cs
@@ -2,13 +2,27 @@
 using OneBus.Domain.Filters;
 using OneBus.Domain.Interfaces.Repositories;
 using OneBus.Infra.Data.DbContexts;
+using System.Linq.Expressions;
 
 namespace OneBus.Infra.Data.Repositories
 {
-    public class LineTimeRepository : BaseRepository<LineTime, BaseFilter>, ILineTimeRepository
+    public class LineTimeRepository : BaseRepository<LineTime, LineTimeFilter>, ILineTimeRepository
     {
         public LineTimeRepository(OneBusDbContext dbContext) : base(dbContext)
         {
+        }
+
+        protected override Expression<Func<LineTime, bool>> ApplyFilter(LineTimeFilter filter)
+        {
+            var value = filter.Value?.ToLower();
+
+            return c =>
+                (filter.DayType == null || c.DayType == filter.DayType) &&
+                (filter.DirectionType == null || c.DirectionType == filter.DirectionType) &&
+                (string.IsNullOrWhiteSpace(value) ||
+                ((c.Line!.Name.ToLower().Contains(value) ||
+                c.Line.Mileage.ToString().Contains(value) ||
+                c.Line.Number.ToLower().Contains(value)) && value != string.Empty));
         }
     }
 }
