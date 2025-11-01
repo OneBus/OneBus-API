0d86917fcf0a6e4ce8226529dd2264fb1aef9a88
Eduardo Rezende <dudumoises2005@gmail.com>
2025-11-01 20:30:49 -0300

    ✨feat: Add Migration
    Updating Vehicle Operation table fields

---
+++ OneBus.Infra.Data/Migrations/20251101221914_UpdateVehicleOperationTable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneBus.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVehicleOperationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "VehicleOperation");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "LineTimeId",
+                table: "VehicleOperation",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "LineTimeId",
+                table: "VehicleOperation",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Date",
+                table: "VehicleOperation",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+    }
+}
